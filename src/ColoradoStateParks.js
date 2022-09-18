c95ce2ccaaed7ef5ac3e14d5cb71df9890967abf
Felix <barosiofelix@gmail.com>
2022-09-18 12:28:34 +0300

    Completed Assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,18 @@
 import React from "react";
-import howManyParks from "./parks/howManyParks";
+// import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/howManyParks";
+import * as RMFunctions from "./parks/RockyMountain";
+// import { trees, wildlife } from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
-
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  )
 }
+
+export default ColoradoStateParks;
+RMFunctions.wildlife();
+console.log(RMFunctions.trees);
+RMFunctions.elevation();
